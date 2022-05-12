a06d46f9759922960a87916997fffa8622ec9e51
leticiaWebs <leticiaaod@gmail.com>
2022-05-12 12:40:25 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-   <h1>Pagina principal</h1>
+    <Navbar/>
   );
 }
 
